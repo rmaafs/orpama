63e5f5a3fbd8695adb5d2174c35dd7ee37a49ccc
F-rancisco-Medina <franciscomedina.05.12.99@gmail.com>
2020-03-06 15:51:48 -0600

    El fondo esta listo

--- script/screen.js
+++ script/screen.js
@@ -1,7 +1,14 @@
 var config = {
     type: Phaser.AUTO,
-    width: 800,
+    width: 1400,
     height: 600,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 300 },
+            debug: false
+        }
+    },
     scene: {
         preload: preload,
         create: create,
@@ -14,8 +21,8 @@ var game = new Phaser.Game(config);
 function preload (){
     this.load.image('sky', '../assets/sky.png');
     this.load.image('star', '../assets/star.png');
-    this.load.spritesheet('p1', 
-    '../assets/amarillo.png',
+    this.load.image('pared', '../assets/pared.png');
+    this.load.spritesheet('p1', '../assets/amarillo.png',
     { frameWidth: 32, frameHeight: 48 }
 );
 
@@ -25,11 +32,14 @@ function create (){
     this.add.image(400, 300, 'sky');
     platforms = this.physics.add.staticGroup();
     
+    platforms.create(700, 575, 'pared').setScale(2).refreshBody();
+
     
+    platforms.create(600, 400, 'pared');
+    platforms.create(50, 250, 'pared');
+    platforms.create(750, 120, 'pared');
 
-
-    this.add.image(10,10, 'star');
-    this.add.image(50,50, 'star');
+    
 
 }
 
